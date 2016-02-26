c79b6bfb8a844b724dd7102622a043c7b6791a7a
Jack Spargo <jack.spargo@gmail.com>
2016-02-26 21:06:34 +0000

    adding temp function

--- speak.sh
+++ speak.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # If no variable passed, say hellp and say the time
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "Hello! My name is Project Mercury" | festival --tts
-    T="$(date +%T)"
+    # %T - 24 hr
+    # %I:%M - HH:mm
+    # %r - 12 hr
+    T="$(date +%I:%M)"
     echo "The time is ${T}" | festival --tts 
 
 # Read the variable passed
 # TODO: some checks that it can be read, or default
+elif [ "$1" = "--temp" ] || [ "$1" = "-t" ]; then
+    temp=$(python getCurrentTemp.py)
+    echo "The current temperature is ${temp} degrees" | festival --tts
 else
     echo "$1" | festival --tts
 fi
