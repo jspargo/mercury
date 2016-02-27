bd034f628b3a5a8edd3638236112ebc056ea1b5a
Jack Spargo <jspargo@deloitte.co.uk>
2016-02-27 15:50:21 +0000

    getting the temp from AneMo - TODO: url in settings of config

--- getCurrentTemp.py
+++ getCurrentTemp.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import requests
+import json
+
 
 def getCurrentTemp():
-    return "12.31"
+    response = requests.get('http://52.19.110.248:8000/latest/')
+    data = json.loads(response.text)
+    temp = data[0]['recorded_temp']
+    date = data[0]['recorded_date']
+    return temp
 
 print getCurrentTemp()
